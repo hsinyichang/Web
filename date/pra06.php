f789a5bccbd9a768e473f0639238050282523095
hsinyichang <f1233772002@gmail.com>
2022-04-25 16:32:19 +0800

    新增老師的內容

--- date/pra06.php
+++ date/pra06.php
@@ -19,7 +19,7 @@
 </head>
 <body>
     <?php
-    $month=4;
+    $month=6;
     ?>
     <table>
         
@@ -32,7 +32,7 @@
         <td>五</td>
         <td>六</td>
     </tr>
-        <?php
+        <!-- <?php
         $firstDay=date("Y-").$month."-1";//當月第一天
         $monthDays=date("t",strtotime($firstDay));//當月天數
         $lastDay=date("Y-").$month."-".$monthDays;//當月最後一天
@@ -46,7 +46,61 @@
             }
                 echo '</tr>';
         }
-        ?>
+        ?> -->
+
+<?php
+
+$firstDay=date("Y-").$month."-1";
+$firstWeekday=date("w",strtotime($firstDay));
+$monthDays=date("t",strtotime($firstDay));
+$lastDay=date("Y-").$month."-".$monthDays;
+$today=date("Y-m-d");
+echo "月份".$month;
+echo "<br>";
+echo "第一天是".$firstDay;
+echo "<br>";
+echo "第一天是星期".$firstWeekday;
+echo "<br>";
+echo "最後一天是".$lastDay;
+echo "<br>";
+echo "當月天數共".$monthDays;
+echo "<br>";
+for($i=0;$i<6;$i++){
+    echo "<tr>";
+    
+    for($j=0;$j<7;$j++){
+        $d=$i*7+($j+1)-$firstWeekday-1;
+        
+        if($d>=0 && $d<$monthDays){
+            $fs=strtotime($firstDay);
+            $shiftd=strtotime("+$d days",$fs);
+            $date=date("d",$shiftd);
+            $w=date("w",$shiftd);
+            $chktoday="";
+            if(date("Y-m-d",$shiftd)==$today){
+                $chktoday='today';
+            }
+            //$date=date("Y-m-d",strtotime("+$d days",strtotime($firstDay)));
+            if($w==0 || $w==6){
+                echo "<td class='weekend $chktoday' >";
+            }else{
+                echo "<td class='workday $chktoday'>";
+            }
+            echo $date;
+            echo "</td>";
+        }else{
+            echo "<td></td>";
+        }
+            
+
+    }
+
+    echo "</tr>";
+}
+
+
+
+?>
     </table>
 </body>
 </html>
