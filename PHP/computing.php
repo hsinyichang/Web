781e81a0b40c684732ac886203609a75c9e8534b
hsinyichang <f1233772002@gmail.com>
2022-05-13 11:13:48 +0800

    計算頁更改

--- PHP/computing.php
+++ PHP/computing.php
@@ -1,15 +1,10 @@
 <?php
-print_r($_GET);
-echo '<br>';
+//計算頁
 
 $height=$_GET['height'];//取出身高值
 $weight=$_GET['weight'];//取出體重值
 
-echo "身高為:".$height;
-echo '<br>';
-echo "體重為:".$weight;
-echo '<br>';
+$bmi=round($weight/(($height/100)*($height/100)),1);//取小數點後一位
 
-$bmi=round($weight/(($height/100)*($height/100)),1);
-echo "BMI值為:".$bmi;
+header("location:result.php?bmi=$bmi");//把值傳遞給結果頁(要用雙引號才能顯示變數)
 ?>
