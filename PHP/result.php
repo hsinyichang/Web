f02a6cf1eed1c14bf3378f4456e4cdb6ef373a77
hsinyichang <f1233772002@gmail.com>
2022-05-13 12:00:55 +0800

    修改內容

--- PHP/result.php
+++ PHP/result.php
@@ -8,13 +8,30 @@
 </head>
 <body>
     <h1 style="font-size: 3rem;text-align:center">
-    <?php
+    你的Bmi值為:<?=$_GET['bmi']?>
+    </h1>
+    <h2 style="text-align:center">
+        判定結果為:<?=$_GET['result']?>
+    </h2>
+    <!-- <?php
         $bmi=$_GET['bmi'];
         echo "你的BMI值為:".$bmi;
+        
+    ?> 直接寫在上面的h1&h2-->
+    
+    <!--請根據BMI值，在畫面上秀出中文的BMI結果或建議-->
+    <!-- <?php
+    if($bmi<18.5){
+        echo "體重過輕，要多攝取營養";
+    }else if($bmi<24 && $bmi>=18.5){
+        echo "體重在正常範圍值，繼續保持";
+    }else{
+        echo "體重過重，要開始健身囉";
+    }
+    ?> 直接寫去計算頁也可以-->
 
-    ?>
     <br>
-    <a href="bmi.html"><button>重新計算</button></a>
-    </h1>
+    <h3 style="text-align:center"><a href="bmi.html"><button>重新計算</button></a></h3> 
+    
 </body>
 </html>
