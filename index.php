79550ac4ebccc59d912c971feb234674f1c0b845
hsinyichang <f1233772002@gmail.com>
2022-05-27 14:58:11 +0800

    連線資料庫row

--- index.php
+++ index.php
@@ -14,24 +14,32 @@
 <body>
     <h1>PHP連線資料庫</h1>
     <?php
-        $dsn="mysql:host=localhost;charset=utf8;dbname=school2";
-        $pdo=new PDO($dsn,'root');
+        /* $dsn="mysql:host=localhost;charset=utf8;dbname=school2";
+        $pdo=new PDO($dsn,'root',''); */
+        $conn=mysqli_connect('localhost','root','','school2');
 
         $sql="SELECT `students`.*,`dept`.`code`,`dept`.`name` as '科系' FROM `students`,`dept` WHERE `dept`.`id`=`students`.`dept`";
 
         // $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_NUM);
                                         //抓取NUM索引的部分
-       
-        $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        $query=mysqli_query($conn,$sql);
+        // $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
                                         //抓取ASSOC的部分
-        echo "<pre>";
-        print_r($rows);
-        echo "</pre>";//用來檢視以上code有無正確
-
-
-        /*     echo $rows[0][3];
-                echo "<br>";
-                echo $rows[0]['birthday']; */
+        //echo var_dump($query);
+        $count=0;
+        while($row=mysqli_fetch_array($query,MYSQLI_BOTH)){
+        if($row['name']=='王鳳如'){
+            echo "<pre>";
+            print_r($row);
+            echo "</pre>";
+        }
+        $count++;
+    }
+    echo $count;
+/*     echo $rows[0][3];
+    echo "<br>";
+    echo $rows[0]['birthday']; */
+    ?>
 
     ?>
 
