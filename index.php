928767c90594cabd1f57bb439735ee29ceb4fabf
hsinyichang <f1233772002@gmail.com>
2022-05-27 12:01:28 +0800

    連線資料庫

--- index.php
+++ index.php
@@ -7,6 +7,26 @@
     <title>PHP連線資料庫</title>
 </head>
 <body>
-    
+    <h1>PHP連線資料庫</h1>
+    <?php
+        $dsn="mysql:host=localhost;charset=utf8;dbname=school2";
+        $pdo=new PDO($dsn,'root');
+
+        $sql="SELECT * FROM `students`,`dept` WHERE `dept`.`id` = `students`.`dept`";
+
+        // $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_NUM);
+                                        //抓取NUM索引的部分
+        
+        $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        echo "<pre>";
+        print_r($rows);
+        echo "</pre>";
+
+
+    ?>
+
+
+
+
 </body>
 </html>
