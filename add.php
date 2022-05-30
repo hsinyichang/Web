41c614811252c2ba04e34f1b72e72f02ad03154f
hsinyichang <f1233772002@gmail.com>
2022-05-30 09:39:33 +0800

    新增學生資料

---
+++ add.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+
+    </style>
+</head>
+<body>
+    <h1>新增學生資料</h1>
+    <form action="" method="post">
+        <label for="">學號:<input type="text" name="" id=""></label><br>
+        <label for="">班級座號:<input type="text" name="" id=""></label><br>
+        <label for="">姓名:<input type="text" name="" id=""></label><br>
+        <label for="">生日:<input type="text" name="" id=""></label><br>
+        <label for="">身分證號碼:<input type="text" name="" id=""></label><br>
+        <label for="">住址:<input type="text" name="" id=""></label><br>
+        <label for="">家長:<input type="text" name="" id=""></label><br>
+        <label for="">電話:<input type="text" name="" id=""></label><br>
+        <label for="">科別:<input type="text" name="" id=""></label><br>
+        <label for="">畢業國中:<input type="text" name="" id=""></label>
+    </form>
+</body>
+</html>
