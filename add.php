b7b7c4e106530b0cfa9b9822a3fe3135c6fa7c99
hsinyichang <f1233772002@gmail.com>
2022-05-30 09:50:38 +0800

    增加css

--- add.php
+++ add.php
@@ -6,6 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>新增學生資料</title>
     <style>
+        *{
+            font-family: '標楷體';
+            text-align: center;
+            color: darkblue;
+            font-weight: bold;
+        }
+        h1{
+            color: purple;
+            text-shadow: 1px 3px 2px lightgray;
+        }
         label{
             display: block;
             padding: 4px;
@@ -20,16 +30,16 @@
 <body>
     <h1>新增學生資料</h1>
     <form action="" method="post">
-        <label for="">學號:<input type="text" name="" id=""></label>
+        <label for="">學號:&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="" id=""></label>
         <label for="">班級座號:<input type="text" name="" id=""></label>
-        <label for="">姓名:<input type="text" name="" id=""></label>
-        <label for="">生日:<input type="text" name="" id=""></label>
-        <label for="">身分證號碼:<input type="text" name="" id=""></label>
-        <label for="">住址:<input type="text" name="" id=""></label>
-        <label for="">家長:<input type="text" name="" id=""></label>
-        <label for="">電話:<input type="text" name="" id=""></label>
-        <label for="">科別:<input type="text" name="" id=""></label>
-        <label for="">畢業國中:<input type="text" name="" id=""></label>
+        <label for="">姓名:&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="" id=""></label>
+        <label for="">生日:&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="" id=""></label>
+        <label for="">身分證碼:<input type="text" name="" id=""></label>
+        <label for="">住址:&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="" id=""></label>
+        <label for="">家長:&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="" id=""></label>
+        <label for="">電話:&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="" id=""></label>
+        <label for="">科別:&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="" id=""></label>
+        <label for="">畢業國中:<input type="text" name="" id=""></label><br>
         <input type="submit" value="新增">
         <input type="reset" value="重置">
     </form>
