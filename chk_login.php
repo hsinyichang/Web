42a5fdfd2b5fc9d92c57dcd5e364fd656b65fcb0
hsinyichang <f1233772002@gmail.com>
2022-06-06 10:52:16 +0800

    簡化登入判斷

--- chk_login.php
+++ chk_login.php
@@ -5,10 +5,13 @@ include "connect.php";
 $acc=$_POST['acc'];
 $pw=$_POST['pw'];
 
-$sql="SELECT * FROM `users` WHERE `acc`=='$acc' && `pw`=='$pw'";
-$user=$pdo->query($sql)->fetch();
+$sql="SELECT count(*) FROM `users` WHERE `acc`='$acc' && `pw`='$pw'";
 
-if($acc==$user['acc'] && $pw==$user['pw']){
+//$user=$pdo->query($sql)->fetch();
+$chk=$pdo->query($sql)->fetchColumn();
+
+//if($acc==$user['acc'] && $pw==$user['pw']){
+if($chk){
     header("location:member_center.php");
 }else{
     header("location:login.php?error=帳號或密碼錯誤");
