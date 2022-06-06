7bed3996783a19fdc18bab78759caad173980e5b
hsinyichang <f1233772002@gmail.com>
2022-06-06 11:22:23 +0800

    會員中心修改

--- member_center.php
+++ member_center.php
@@ -9,18 +9,19 @@
 <body>
 <nav><a href="logout.php">登出</a></nav>
     <h1>會員中心</h1>
-    <?php session_start();//有session的地方都要加這行
+    <?php
+    include "connect.php";//session_start()已寫在裡面了
     ?>
-
+    
     歡迎<?=$_SESSION['user'];?>,祝你有美好的一天
 
     <?php
-    include "connect.php";
     $sql="SELECT * FROM `users` where acc='{$_SESSION['user']}'";
     $user=$pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    echo '<hr>';
     echo 'ID:'.$user['id'].'<br>';
     echo '帳號:'.$user['acc'].'<br>';
-    echo '密碼:'.$user['pw'].'<br>';
+    echo '密碼:******<br>';
     echo '密碼提示:'.$user['passnote'].'<br>';
     echo '生日:'.$user['birthday'].'<br>';
     echo 'E-mail:'.$user['email'].'<br>';
