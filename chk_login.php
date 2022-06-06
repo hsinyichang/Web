9db6182e2ade4d64f386103db9a07793a9fc8b57
hsinyichang <f1233772002@gmail.com>
2022-06-06 09:57:33 +0800

    完成登入驗證

--- chk_login.php
+++ chk_login.php
@@ -1,9 +1,16 @@
 <?php
 //檢查帳密是否正確
 
-if(true){
+include "connect.php";
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+
+$sql="SELECT * FROM `users` WHERE `acc`=='$acc' && `pw`=='$pw'";
+$user=$pdo->query($sql)->fetch();
+
+if($acc==$user['acc'] && $pw==$user['pw']){
     header("location:member_center.php");
 }else{
-    header("location:login.php");
+    header("location:login.php?error=帳號或密碼錯誤");
 }
 ?>
