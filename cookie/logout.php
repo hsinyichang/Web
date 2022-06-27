35e356e450b9e72a9a15b9565a83d6a1e53c4459
HsinYiChang <f1233772002@gmail.com>
2022-06-27 12:02:13 +0800

    宣告類別並實例化

--- cookie/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(isset($_COOKIE['login'])){   //真正可以讓cookie刪除
-setcookie("login","",time()-1);
-}
-header("location:index.php");
-?>
--- session/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();//要用到session的地方都要加這行
-unset($_SESSION['login']);
-header("location:index.php");
-?>
