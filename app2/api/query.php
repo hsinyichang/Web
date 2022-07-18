1e677aaf39187bf77ba7d6ff34cbadcb60fc3613
gina <f1233772002@gmail.com>
2022-07-18 11:26:39 +0800

    完成使用ajax來新增查詢資料

--- app2/api/query.php
+++ app2/api/query.php
@@ -1,5 +1,12 @@
 <?php
 //處理查詢資料的請求
+$classroom=$_GET['classroom'];//index裡的班級
+$dsn="mysql:host=localhost;charset=utf8;dbname=students";
+$pdo=new PDO($dsn,'root','');
 
+$sql="select * from students where classroom='{$classroom}'";
+$rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($rows);
 
 ?>
